b96b7f6224d069cfe28304888ddb1757c51c197f
Jan Richter <jrichter@redhat.com>
2025-01-16 15:47:58 +0100

    remove debug code

--- foo.sh
+++
@@ -1,6 +0,0 @@
-cd templates
-
-find . -regextype egrep -regex '.*ya?ml$'|while read file; do
-  # sed -i 's/: \s*['\''"]\{0\}\([^'\''"]*{{.*}}.*\)\s*$/: '\''\1'\''/' $file
-  sed -i '/^\(\s*\)\({%-.*\)$/d' $file
-done
